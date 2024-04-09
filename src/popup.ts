7daf00da20b86992905bc9a51d3946ba8d074d64
Rachel Shu <rachel@rachelshu.com>
2024-04-09 13:44:08 -0500

    add options to popup, disable main function for dev

--- src/popup.ts
+++ src/popup.ts
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const diacritizeBtn = document.getElementById('diacritizeBtn');
-  
+    const optionsBtn = document.getElementById('optionsBtn');
+
     if (diacritizeBtn) {
       diacritizeBtn.addEventListener('click', () => {
-        // Send a message to the content script to start diacritization
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          if (tabs.length > 0 && tabs[0].id !== undefined) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'diacritize' });
-          }
-        });
+      // Send a message to the content script to start diacritization
+      alert('Not implemented');
+      //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      //     if (tabs.length > 0 && tabs[0].id !== undefined) {
+      //       chrome.tabs.sendMessage(tabs[0].id, { action: 'diacritize' });
+      //     }
+      //   });
+      });
+    }
+
+    if (optionsBtn) {
+      optionsBtn.addEventListener('click', () => {
+        // Open the options page
+        if (chrome.runtime.openOptionsPage) {
+          // New way to open options pages, if supported (Chrome 42+).
+          chrome.runtime.openOptionsPage();
+        } else {
+          // Reasonable fallback.
+          window.open(chrome.runtime.getURL('public/options.html'));
+        }
       });
     }
   });
