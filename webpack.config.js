cfc2387cf9163fea5b9bb49891f6e46b4d40dcc3
Rachel Shu <rachel@rachelshu.com>
2024-04-10 13:45:22 -0500

    add web-ext-reloader configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtReloader = require('webpack-ext-reloader');
 
-
 module.exports = {
+
   mode: 'development',
+  devtool: 'cheap-module-source-map',
   watch: true,
+
   entry: {
     background: './src/background.ts',
     options: './src/options.ts',
@@ -13,10 +15,12 @@ module.exports = {
     content: './src/content.ts'
     // Add other entry points if needed
   },
+
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+
   module: {
     rules: [
       {
@@ -26,11 +30,20 @@ module.exports = {
       },
     ],
   },
+
   resolve: {
     extensions: ['.ts', '.js'],
   },
+
   plugins: [
-    new ExtReloader(),
+    new ExtReloader({
+      entries: { 
+        contentScript: ['content'],
+        background: 'background',
+        extensionPage: ['popup', 'options'],
+      }
+    }),
+
     new CopyWebpackPlugin({
       patterns: [
         { from: 'public', to: '.' },
