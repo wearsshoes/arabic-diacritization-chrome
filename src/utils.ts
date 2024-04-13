931ebc090e2bd019d74853d7c7db59f8d4a3aa6b
Rachel Shu <rachel@rachelshu.com>
2024-04-13 14:40:54 -0500

    rewrite calculateHash, now accepts a single string instead of only arrays of them.

--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,21 @@
 import xxhash from 'xxhash-wasm'
 
 
-export async function calculateHash(input: string[]): Promise<string[]> {
+export async function calculateHash(input: string | string[]): Promise<string | string[]> {
     // Creates the WebAssembly instance.
     const hasher = await xxhash();
-    const response: string[] = []
+    const responseArray: string[] = []
   
     // Calculates the 64-bit hash.
-    input.forEach(inputString => {
-      const hash64Hex = hasher.h64ToString(inputString);
-      response.push(hash64Hex)
-    });
-    
-    return response;
+    if (Array.isArray(input)) {
+      input.forEach(inputString => {
+        const hash64Hex = hasher.h64ToString(inputString);
+        responseArray.push(hash64Hex);
+      });
+      return responseArray;
+    } else {
+        const hash64Hex = hasher.h64ToString(input);
+        return hash64Hex;
+    }
+
   }
