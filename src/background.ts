685d7517260e04e268512bc91b95d8d06e4bb574
Rachel Shu <rachel@rachelshu.com>
2024-04-13 22:36:45 -0500

    move anthropic API - related functions to anthropicCaller.ts

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,11 @@
 import Anthropic from '@anthropic-ai/sdk';
 import arabizi from './arabizi.json';
 import prompts from './defaultPrompts.json';
-import Bottleneck from 'bottleneck'
+// import Bottleneck from 'bottleneck'
 import { calculateHash, getAPIKey } from './utils';  
-import { Model, Models, Prompt, TransliterationDict, ProcessorResponse, TextElement, SysPromptTokenCache } from './types';
+// import { Model, Models, Prompt, TransliterationDict, ProcessorResponse, TextElement, SysPromptTokenCache } from './types';
+import { Prompt, TransliterationDict, ProcessorResponse, TextElement } from './types';
+import { anthropicLimiter, claude, defaultModel, getStoredPromptTokenCount, anthropicAPICall, countSysPromptTokens, escalateModel } from './anthropicCaller'
 
 // Check whether new version is installed
 chrome.runtime.onInstalled.addListener(function(details){
@@ -39,33 +41,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-const claude: Models = {
-  haiku: {
-    currentVersion: "claude-3-haiku-20240307",
-    level: 0
-  },
-  sonnet: { 
-    currentVersion: "claude-3-sonnet-20240229",
-    level: 1
-  },
-  opus: {
-    currentVersion: "claude-3-opus-20240229",
-    level: 2
-  }
-};
-
-const defaultModel: Model = claude.haiku;
-
-function escalateModel (model: Model, n: number) : Model {
-  // return the model whose level is one higher than the input model using map
-    const mPlusOne = Object.values(claude).find((m) => m.level === model.level + n);
-    if (mPlusOne) {
-      return mPlusOne;
-    } else {
-      return model;
-    }
-} 
-
 const defaultPrompt: Prompt = prompts[0];
 
 async function getPrompt(): Promise<Prompt> {
@@ -81,94 +56,122 @@ async function getPrompt(): Promise<Prompt> {
   });
 }
 
-// Rate-limited Anthropic API call function
-const anthropicLimiter = new Bottleneck({
-  maxConcurrent: 3,
-  minTime: 1500
-});
+// // Rate-limited Anthropic API call function
+// const anthropicLimiter = new Bottleneck({
+//   maxConcurrent: 3,
+//   minTime: 1500
+// });
 
-async function anthropicAPICall(params: Anthropic.MessageCreateParams, key?: string, hash?: string): Promise<any> {
+// const claude: Models = {
+//   haiku: {
+//     currentVersion: "claude-3-haiku-20240307",
+//     level: 0
+//   },
+//   sonnet: { 
+//     currentVersion: "claude-3-sonnet-20240229",
+//     level: 1
+//   },
+//   opus: {
+//     currentVersion: "claude-3-opus-20240229",
+//     level: 2
+//   }
+// };
+
+// const defaultModel: Model = claude.haiku;
+
+// function escalateModel (model: Model, n: number) : Model {
+//   // return the model whose level is one higher than the input model using map
+//     const mPlusOne = Object.values(claude).find((m) => m.level === model.level + n);
+//     if (mPlusOne) {
+//       return mPlusOne;
+//     } else {
+//       return model;
+//     }
+// } 
+
+// async function anthropicAPICall(params: Anthropic.MessageCreateParams, key?: string, hash?: string): Promise<any> {
   
-  // get the API key if it's not provided
-  const apiKey = key || await getAPIKey();
-  if (!apiKey) {
-    // should rewrite to pass getAPIKey error to the caller
-    throw new Error('API key not set');
-  }
+//   // get the API key if it's not provided
+//   const apiKey = key || await getAPIKey();
+//   if (!apiKey) {
+//     // should rewrite to pass getAPIKey error to the caller
+//     throw new Error('API key not set');
+//   }
 
-  const anthropic = new Anthropic({ apiKey: apiKey });
-  console.log('Queued job', hash); 
-  return anthropicLimiter.schedule(async () => {
-    try {
-      console.log('Sent job', hash);
-      const result = await anthropic.messages.create(params);
-      console.log('Received result for:', hash);
-      return result;
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error message:', error.message);
-      }
-      throw error;
-    }
-  });
-}
+//   const anthropic = new Anthropic({ apiKey: apiKey });
+//   console.log('Queued job', hash); 
+//   return anthropicLimiter.schedule(async () => {
+//     try {
+//       console.log('Sent job', hash);
+//       const result = await anthropic.messages.create(params);
+//       console.log('Received result for:', hash);
+//       return result;
+//     } catch (error) {
+//       if (error instanceof Error) {
+//         console.error('Error message:', error.message);
+//       }
+//       throw error;
+//     }
+//   });
+// }
 
-// Check number of system prompt tokens, look up in cache, or call API
-async function countSysPromptTokens(prompt: string, model?: string): Promise<number> {
-  const modelUsed = model || defaultModel.currentVersion;
-  const promptHash = await calculateHash(prompt) as string;
+// // Check number of system prompt tokens, look up in cache, or call API
+// async function countSysPromptTokens(prompt: string, model?: string): Promise<number> {
+//   const modelUsed = model || defaultModel.currentVersion;
+//   const promptHash = await calculateHash(prompt) as string;
 
-  const storedTokenCount = await getStoredPromptTokenCount(promptHash, modelUsed);
-  if (storedTokenCount !== null) {
-    return storedTokenCount;
-  }
+//   const storedTokenCount = await getStoredPromptTokenCount(promptHash, modelUsed);
+//   if (storedTokenCount !== null) {
+//     return storedTokenCount;
+//   }
 
-  const msg = await anthropicAPICall({
-    model: modelUsed,
-    max_tokens: 1,
-    temperature: 0,
-    messages: [
-      {
-        role: "user",
-        content: [
-          {
-            type: "text",
-            text: prompt
-          }
-        ]
-      }
-    ]
-  });
+//   const msg = await anthropicAPICall({
+//     model: modelUsed,
+//     max_tokens: 1,
+//     temperature: 0,
+//     messages: [
+//       {
+//         role: "user",
+//         content: [
+//           {
+//             type: "text",
+//             text: prompt
+//           }
+//         ]
+//       }
+//     ]
+//   });
 
-  const sysPromptTokens: number = msg.usage.input_tokens;
-  saveSysPromptTokenCount(promptHash, modelUsed, sysPromptTokens);
+//   const sysPromptTokens: number = msg.usage.input_tokens;
+//   saveSysPromptTokenCount(promptHash, modelUsed, sysPromptTokens);
 
-  return sysPromptTokens;
-}
+//   return sysPromptTokens;
+// }
 
-async function getStoredPromptTokenCount(promptHash: string, model: string): Promise<number | null> {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get('savedResults', (data: { savedResults?: SysPromptTokenCache[] }) => {
-      if (Array.isArray(data.savedResults)) {
-        const storedPrompt = data.savedResults.find(
-          (result) => result.hash === promptHash && result.model === model
-        );
-        if (storedPrompt) {
-          return resolve(storedPrompt.tokens);
-        }
-      }
-      resolve(null);
-    });
-  });
-}
+// async function getStoredPromptTokenCount(promptHash: string, model: string): Promise<number | null> {
+//   return new Promise((resolve) => {
+//     chrome.storage.sync.get('savedResults', (data: { savedResults?: SysPromptTokenCache[] }) => {
+//       if (Array.isArray(data.savedResults)) {
+//         const storedPrompt = data.savedResults.find(
+//           (result) => result.hash === promptHash && result.model === model
+//         );
+//         if (storedPrompt) {
+//           return resolve(storedPrompt.tokens);
+//         }
+//       }
+//       resolve(null);
+//     });
+//   });
+// }
 
-function saveSysPromptTokenCount(promptHash: string, model: string, tokens: number): void {
-  chrome.storage.sync.get('savedResults', (data: { savedResults?: SysPromptTokenCache[] }) => {
-    const savedResults = data.savedResults || [];
-    savedResults.push({ hash: promptHash, model, tokens });
-    chrome.storage.sync.set({ savedResults });
-  });
-}
+// function saveSysPromptTokenCount(promptHash: string, model: string, tokens: number): void {
+//   chrome.storage.sync.get('savedResults', (data: { savedResults?: SysPromptTokenCache[] }) => {
+//     const savedResults = data.savedResults || [];
+//     savedResults.push({ hash: promptHash, model, tokens });
+//     chrome.storage.sync.set({ savedResults });
+//   });
+// }
+
 // Async worker for API call
 async function processTranslationBatches(method: string, cache: ProcessorResponse[], translationBatches: { text: string; elements: TextElement[] }[]): Promise<ProcessorResponse[]> {
   const texts = translationBatches.map((batch) => batch.text);
