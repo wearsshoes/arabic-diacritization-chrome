b0eba4ca38278536646b7c1bdd68747571b9ecfa
Rachel Shu <rachel@rachelshu.com>
2024-04-15 12:06:55 -0500

    Add jest.setup.js file and update jest.config.js to include setupFiles

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     preset: 'ts-jest',
+    setupFiles: ['./jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     globals: {
       'ts-jest': {
         tsconfig: 'tsconfig.json'
       }
-    }
+    },
   };
   
