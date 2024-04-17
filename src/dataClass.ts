0d78006b27683cc14b865c2dfe0413d571396c0a
Rachel Shu <rachel@rachelshu.com>
2024-04-17 00:28:02 -0700

    add build() method to WebPageDiacritizationData rather than direct creation

--- src/dataClass.ts
+++ src/dataClass.ts
@@ -2,19 +2,19 @@ import { calculateHash } from "./utils";
 
 export interface PageMetadata {
     pageUrl: string,
-  lastVisited: Date,
-  contentSignature: string,
-  structuralMetadata: string,
+    lastVisited: Date,
+    contentSignature: string,
+    structuralMetadata: string,
 }
 
 export interface TextNode {
-  elementId: string;
-  index: number
-  text: string;
+    elementId: string;
+    index: number
+    text: string;
 }
 
 export interface NodeHashDict {
-  [nodeHash: string]: TextNode
+    [nodeHash: string]: TextNode
 }
 
 // it's like, not inconvceivable that you just transmit the entire webpage into background.ts
@@ -23,60 +23,63 @@ export class WebPageDiacritizationData {
     private diacritizations?: {
         [method: string]: NodeHashDict
     };
+    
+    constructor(
+        public id: string,
+        public metadata: PageMetadata,
+    ) {};
 
-  constructor(  
-    public pageUrl: string,
-    public metadata: PageMetadata,
-    websiteText: TextNode[]
-    ) { 
-      this.createOriginal(websiteText)
-    }
-          
-  async createOriginal(websiteText: TextNode[]) {
-    // aaa this calls calculateHash like a thousand times 
-    const nodeHashes = await Promise.all(websiteText.map( (textNode) => {
-      const hash = calculateHash(textNode.text)
-      return hash
-    }));
+    static async build(
+        metadata: PageMetadata,
+    ) {
+        const id = await calculateHash(metadata.pageUrl)
+        return new WebPageDiacritizationData(id, metadata)      
+    };
+    
+    async createOriginal(websiteText: TextNode[]) {
+    const textlist = websiteText.map((textNode) => (textNode.elementId + textNode.text))
+    const nodeHashes = await calculateHash(textlist)
+  
+    const original = nodeHashes.reduce((dict, nodeHash, index) => {
+      dict[nodeHash] = websiteText[index];
+      return dict;
+    }, {} as NodeHashDict);
+    this.original = original;
+    };
 
-    this.original = websiteText.map((textNode, index) => {
-      const nodeHash = nodeHashes[index]
-      return {[nodeHash]: textNode}
-    });
-  }
 
-  async addDiacritization(diacritizedText: TextNode[], method: string) {
-    if (this.original === undefined) {
-      throw new Error('Original text not created yet.');
-    } else {
-      const original = this.original;
+    async addDiacritization(diacritizedText: TextNode[], method: string) {
+        if (this.original === undefined) {
+            throw new Error('Original text not created yet.');
+        } else {
+            const original = this.original;
             const diacritization: NodeHashDict = {};
             Object.keys(original).forEach((key, index) => {
                 diacritization[key] = diacritizedText[index];
             });
             console.log('Adding diacritization:', diacritization);
-      if (this.diacritizations === undefined) {
+            if (this.diacritizations === undefined) {
                 this.diacritizations = { [method]: diacritization };
-      } else {
+            } else {
                 this.diacritizations[method] = diacritization;
-      }
+            }
+        }
     }
-  }
 
     getDiacritization(method: string): NodeHashDict {
-    if (this.diacritizations === undefined) {
-      throw new Error('Diacritizations not created yet.');
-    } else {
+        if (this.diacritizations === undefined) {
+            throw new Error('Diacritizations not created yet.');
+        } else {
             const diacritization = this.diacritizations[method];
-      if (diacritization === undefined) {
-        throw new Error('Diacritization method not found.');
-      } else {
+            if (diacritization === undefined) {
+                throw new Error('Diacritization method not found.');
+            } else {
                 return diacritization;
-      }
+            }
+        }
     }
-  }
-  updateLastVisited(date: Date): void {
-      this.metadata.lastVisited = date
-  }
+    updateLastVisited(date: Date): void {
+        this.metadata.lastVisited = date
+    }
 
 }
