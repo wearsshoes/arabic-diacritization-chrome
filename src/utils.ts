391a6206e4d0c2e176c8701e5d941568b2ef6846
Rachel Shu <rachel@rachelshu.com>
2024-04-21 08:44:21 -0700

    handle empty string cheaply

--- src/utils.ts
+++ src/utils.ts
@@ -6,13 +6,17 @@ export async function calculateHash(input: string[]): Promise<string[]>;
 export async function calculateHash(input: string | string[]): Promise<string | string[]> {
   // Creates the WebAssembly instance.
   const hasher = await xxhash();
-  
+
   // Calculates the 64-bit hash.
   if (Array.isArray(input)) {
     const responseArray: string[] = []
     input.forEach(inputString => {
-      const hash64Hex = hasher.h64ToString(inputString);
-      responseArray.push(hash64Hex);
+      if (inputString === '') {
+        responseArray.push('')
+      } else {
+        const hash64Hex = hasher.h64ToString(inputString);
+        responseArray.push(hash64Hex);
+      }
     });
     return responseArray;
   } else {
@@ -47,16 +51,16 @@ export async function getAPIKey(): Promise<string> {
 
 export function chromeStorageGet<T>(key: string): Promise<{ [key: string]: T }> {
   return new Promise((resolve, reject) => {
-      chrome.storage.sync.get([key], result => {
-          chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve(result);
-      });
+    chrome.storage.sync.get([key], result => {
+      chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve(result);
+    });
   });
 }
 
 export function chromeStorageSet(items: { [key: string]: any }): Promise<void> {
   return new Promise((resolve, reject) => {
-      chrome.storage.sync.set(items, () => {
-          chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve();
-      });
+    chrome.storage.sync.set(items, () => {
+      chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve();
+    });
   });
 }
