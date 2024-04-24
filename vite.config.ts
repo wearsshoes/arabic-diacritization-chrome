9008b5b2571aa1a6f57ceacd07120922f0165cb9
Rachel Shu <rachel@rachelshu.com>
2024-04-24 15:56:03 -0700

    realign structure to crxjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
-import copy from 'rollup-plugin-copy';
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    copy({
-      targets: [
-        { src: 'src/components/options.html', dest: 'dist/assets' },
-        { src: 'src/components/popup.html', dest: 'dist/assets' },
-      ],
-      hook: 'writeBundle',
-    }),
+    crx({ manifest }),
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/main.tsx'),
-        popup: resolve(__dirname, 'src/components/popup.tsx'),
-        options: resolve(__dirname, 'src/components/options.tsx'),
-        background: resolve(__dirname, 'src/background.ts'),
-        content: resolve(__dirname, 'src/content.ts')
-      },
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
 });
