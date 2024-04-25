955a50e25020392f891679732a841d1e2872d190
Rachel Shu <rachel@rachelshu.com>
2024-04-25 11:53:24 -0700

    remove other tailwind config stuff

--- src/popup/extensionPopup.tsx
+++ src/popup/extensionPopup.tsx
@@ -6,7 +6,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../assets/theme';
 import Fonts from '../assets/fonts';
 import './extensionPopup.css'
-// import '../assets/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
