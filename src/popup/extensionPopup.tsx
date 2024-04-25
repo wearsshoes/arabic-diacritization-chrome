8a9382d01b2db3f9ad61d9e9449b87c5d5848778
Rachel Shu <rachel@rachelshu.com>
2024-04-24 17:34:06 -0700

    reimplement all extensionPopup features

--- src/popup/extensionPopup.tsx
+++ src/popup/extensionPopup.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './extensionPopup.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <h1>ArabEasy</h1>
     <App />
   </React.StrictMode>,
 )
