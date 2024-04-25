9960433326604486814099d91611176eb53456d9
Rachel Shu <rachel@rachelshu.com>
2024-04-24 17:11:04 -0700

    simplify optionsPage files

--- src/options/optionsPage.tsx
+++ src/options/optionsPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './optionsPage.css';
 import LLMOptions from './llmOptions';
 import APIKeyForm from './apiKeyForm';
 import DataManagement from './dataManagement';
@@ -15,4 +17,8 @@ const Options: React.FC = () => {
   );
 };
 
-export default Options;
+ReactDOM.createRoot(document.getElementById('options-root')!).render(
+    <React.StrictMode>
+        <Options />
+    </React.StrictMode>,
+)
