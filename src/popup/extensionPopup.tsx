f6c937fa72c6980928cafd9b5fa83794946458a2
Rachel Shu <rachel@rachelshu.com>
2024-05-01 16:25:36 -0700

    roll App.tsx back into extensionPopup

--- src/popup/extensionPopup.tsx
+++ src/popup/extensionPopup.tsx
@@ -1,10 +1,216 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { Prompt } from '../common/types';
+import { getAPIKey } from '../background/datamanager';
+
 import theme from '../assets/theme';
 import Fonts from '../assets/fonts';
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  SimpleGrid,
+  Button,
+  Card,
+  Heading,
+  HStack,
+  Select,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  VStack,
+  Center,
+  Text
+} from '@chakra-ui/react';
+
+const App: React.FC = () => {
+
+  const [pageLanguage, setPageLanguage] = useState('');
+  const [selectedPrompt, setSelectedPrompt] = useState('');
+  const [promptLength, setPromptLength] = useState(0);
+  const [characterCount, setCharacterCount] = useState(0);
+  const [outputTokenCount, setOutputTokenCount] = useState(0);
+  const [model, setModel] = useState('');
+  const [costEstimate, setCostEstimate] = useState('');
+  const [diacritizeStatus, setDiacritizeStatus] = useState('');
+
+  useEffect(() => {
+    // Check API key
+    // TODO: just get it from the background worker instead
+    const apiKey = getAPIKey()
+    if (!apiKey) {
+      const button = document.createElement('Button');
+      button.textContent = 'Please set your API key in the options page.';
+      document.getElementById('main')?.replaceChildren(button);
+      button.addEventListener('click', () => chrome.runtime.openOptionsPage());
+    }
+
+    // Get website data
+    getWebsiteData();
+
+    // Get selected prompt
+    getSelectedPrompt();
+
+    // Update model display
+    setModel('Claude Haiku');
+
+  }, []);
+
+  const getWebsiteData = async () => {
+    try {
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      if (tab.id === undefined) throw new Error('No active tab found');
+
+      const response = await chrome.tabs.sendMessage(tab.id, { action: 'getWebsiteData' });
+      
+      // if cache exists for selected task, inform user.
+
+      if (response.language) {
+        const languageNamesInEnglish = new Intl.DisplayNames(['en'], { type: 'language' });
+        const languageNamesInArabic = new Intl.DisplayNames(['ar'], { type: 'language' });
+        const lang = languageNamesInEnglish.of(response.language) || 'unknown';
+        const lang_ar = languageNamesInArabic.of(response.language) || 'unknown';
+        setPageLanguage(lang + ' (' + lang_ar + ')');
+      };
+  
+      setCharacterCount(response.characterCount);
+      setOutputTokenCount(response.batches);
+    } catch (error) {
+      console.error('Failed to get complete website data:', error);
+    }
+  };
+
+  const getSelectedPrompt = () => {
+    chrome.storage.sync.get(['selectedPrompt'], (data: { selectedPrompt?: Prompt }) => {
+      if (data.selectedPrompt) {
+        setSelectedPrompt(data.selectedPrompt.name);
+        chrome.runtime.sendMessage(
+          { action: 'getSystemPromptLength', prompt: data.selectedPrompt.text },
+          (response) => {
+            if (response) {
+              setPromptLength(response);
+            }
+          }
+        );
+      }
+    });
+  };
+
+  const beginDiacritization = async (method: string) => {
+    try {
+      setDiacritizeStatus('Diacritizing, see progress bar modal...');
+      const response = await chrome.runtime.sendMessage({ action: 'sendToDiacritize', method });
+      console.log(`${method} response:`, response);
+      setDiacritizeStatus('Diacritization complete, page updated.');
+    } catch (error) {
+      console.error(`Error in ${method}:`, error);
+      setDiacritizeStatus('Error diacritizing:' + error);
+    }
+  };
+
+  const calculateCost = () => {
+    if (outputTokenCount && characterCount && promptLength) {
+      const costEstimate = calculateCostEstimate();
+      const costInDollars = costEstimate.toFixed(2);
+      setCostEstimate(`Estimated cost: $${costInDollars}`);
+    } else {
+      setCostEstimate('Estimated cost: Unknown');
+    }
+  };
+
+  const calculateCostEstimate = (): number => {
+    const inputCost = 0.25 / 1000000;
+    const inputSubtotal = (promptLength * outputTokenCount + characterCount) * inputCost;
+    const outputCost = 1.25 / 1000000;
+    const outputSubtotal = characterCount * 2.3 * outputCost;
+    const totalCostPlusTax = (inputSubtotal + outputSubtotal) * 1.1;
+    return totalCostPlusTax;
+  };
+
+  return (
+    <Card bg='#c2a25d' padding='2' width='100%'>
+      <VStack spacing={2} align="start">
+
+        <Card bg='#fbeed7' padding='2' width='100%'>
+          <Center>
+            <VStack>
+            <Heading fontFamily={'basmala'} padding={2} marginTop={5} marginBottom={0} lineHeight={0}>ArabEasy</Heading>
+            <Text fontFamily={'arabic'} fontSize='xl' fontWeight={900} marginBottom={5} lineHeight={1}>بتِحكي عَرَبِيْزِي؟</Text>
+            </VStack>
+          </Center>
+          <Card padding='2'>
+            <Text>This extension adds diacritics (taškīl) to Arabic text via Claude Haiku. Remember to add your Anthropic API Key on the options page.</Text>
+            <Button size='xs' onClick={() => chrome.runtime.openOptionsPage()}>Open Options Page</Button>
+          </Card>
+        </Card>
+
+        <Card bg='#fbeed7' width={'100%'}>
+          <Accordion allowToggle>
+            <AccordionItem width='100%'>
+              <AccordionButton justifyContent="center">
+                <Heading size='md'>Page information</Heading>
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel padding='2'>
+                <SimpleGrid columns={2} spacing={2} marginBottom={2}>
+                  <Card>
+                    <Text fontWeight={'bold'}>Page language: </Text>
+                    <Text>{pageLanguage}</Text>
+                  </Card>
+                  <Card>
+                    <Text fontWeight={'bold'}>Active prompt: </Text>
+                    <Text>"{selectedPrompt}"</Text>
+                  </Card>
+                  <Card>
+                    <Text fontWeight={'bold'}>Prompt length: </Text>
+                    <Text>{promptLength} tokens</Text>
+                  </Card>
+                  <Card>
+                    <Text fontWeight={'bold'}>Characters on page: </Text>
+                    <Text>{characterCount || 'NaN'}</Text>
+                  </Card>
+                  <Card>
+                    <Text fontWeight={'bold'}>Estimated output: </Text>
+                    <Text>{outputTokenCount || 'NaN'} tokens</Text>
+                  </Card>
+                  <Card>
+                    <Text fontWeight={'bold'}>Model used: </Text>
+                    <Text>{model}</Text>
+                  </Card>
+                </SimpleGrid>
+                <Card>
+                  <HStack>
+                    <Text fontWeight={'bold'}>Estimated cost:</Text>
+                    <Button size='xs' onClick={calculateCost}>Calculate</Button>
+                  </HStack>
+                  <Text>{costEstimate}</Text>
+                </Card>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </Card>
+
+        <Card bg='#fbeed7' width='100%' padding='2'>
+          <Center>
+            <Heading size='md' marginBottom={2}>Task</Heading>
+          </Center>
+          <Card padding='2'>
+            <HStack>
+              <Select size='sm' id="diacritizationSelector">
+                <option value="diacritize">Full Diacritization</option>
+                <option value="arabizi">Arabizi</option>
+              </Select>
+              <Button size='sm' onClick={() => beginDiacritization('diacritize')}>Start</Button>
+            </HStack>
+              <Text>{diacritizeStatus}</Text>
+          </Card>
+        </Card>
+
+      </VStack>
+    </Card>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
