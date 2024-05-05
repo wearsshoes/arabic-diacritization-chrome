19b3f72219f42c16bfdb716528ec80d3704a93ca
Rachel Shu <rachel@rachelshu.com>
2024-05-05 01:04:10 -0600

    some arabizi fixes, lots of todos

--- src/background/arabizi.ts
+++ src/background/arabizi.ts
@@ -1,46 +1,50 @@
 import arabizi from './arabizi.json';
 
 interface TransliterationDict {
-    [key: string]: string[];
-  }  
+  [key: string]: string[];
+}
 
 export function arabicToArabizi(texts: string[], dialect: string = "msa"): string[] {
-    console.log('Transliterating', texts);
-    
-    let transliterationDict: TransliterationDict
-    if (dialect === "msa") {
-      transliterationDict = arabizi.transliteration
-    }
+  console.log('Transliterating', texts);
 
-    return texts.map(arabicText => {
-      if (arabicText && arabicText.length > 0) {
-        // parse definite article
-        // alParse(arabicText)
-        
-        return arabicText  
-          // remove sukoon
-          .replace(/[ْ]/g, '') 
-          
-          // replace all cases of shadda with previous letter
-          .replace(/([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, '$1$1$2') 
-          
-          // map remaining arabic characters according to transliterationDict
-          .split('')
-          .map(char => transliterationDict[char]?.[0] || char)
-          .join('')
-
-          // might want to post-parse weird vowel clusters as a kludge
-      } else {return ''}
-      
-    });
+  let transliterationDict: TransliterationDict
+  if (dialect === "msa") {
+    transliterationDict = arabizi.transliteration
   }
 
-// TODO: identify al-, add dash
-// TODO: sun/moon transformation: al-sudan == as-sudan
-// (maybe this should be an optional flag)
+  return texts.map(arabicText => {
+    if (arabicText && arabicText.length > 0) {
+
+      return arabicText
+        // use shadda and sukkoon to do sun/moon transformation
+        .replace(/(اَ?ل)([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, 'a$2-$2$3')
+        .replace(/([\u0621-\u0652])(اَ?لْ?)/g, '$1\'al-')
+        .replace(/([\s])(اَ?لْ?)/g, '$1al-')
+
+        // replace all other cases of shadda with previous letter
+        .replace(/([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, '$1$1$2')
+
+        // remove remaining sukoon
+        .replace(/[ْ]/g, '')
+
+        // map remaining arabic characters according to transliterationDict
+        .split('')
+        .map(char => transliterationDict[char]?.[0] || char)
+        .join('')
+
+      // might want to post-parse weird vowel clusters as a kludge
+    } else { return '' }
+
+  });
+}
 
 // TODO: joined vowels -- fii instead of fiy, etc
-// e.g. 'arabiyya
+// e.g. 'arabiyyat
+// y and w at beginning of word
+// al- works sometimes and not other times in a weird and inconsistent way; maybe different unicode pts?
+// al gets dash only if near beginning of word (define a list of prefixes...?)
+// treat Allah special
+
 
 // TODO: punctuation (add in arabizi.json)
 
