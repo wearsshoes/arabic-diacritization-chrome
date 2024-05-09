f688c6b7fb371578285be0d5a8905d98bc5f9ae4
Rachel Shu <rachel@rachelshu.com>
2024-05-09 14:30:41 -0600

    rewrite arabizi.ts to take and return TextNode[] instead of string[]

--- src/background/arabizi.ts
+++ src/background/arabizi.ts
@@ -5,39 +5,34 @@ interface TransliterationDict {
   [key: string]: string[];
 }
 
-export function arabicToArabizi(texts: string[], dialect: string = "msa"): string[] {
-  console.log('Transliterating', texts);
+export function arabicToArabizi(textNodes: TextNode[], dialect: string = "msa"): TextNode[] {
 
   let transliterationDict: TransliterationDict
-  if (dialect === "msa") {
-    transliterationDict = arabizi.transliteration
+  switch (dialect) {
+    case "msa":
+      transliterationDict = arabizi.transliteration
   }
 
-  return texts.map(arabicText => {
-    if (arabicText && arabicText.length > 0) {
+  function transliterate(text: string): string {
+    // use shadda and sukkoon to do sun/moon transformation
+    return text.replace(/اللَّه/g, 'Allah')
+      .replace(/(اَ?ل)([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, 'a$2-$2$3')
+      // .replace(/([\u0621-\u0652])(اَ?لْ?)/g, '$1\'l-')
+      .replace(/(^|\s)(اَ?لْ?)/g, '$1al-')
 
-      return arabicText
-        // use shadda and sukkoon to do sun/moon transformation
-        .replace(/اللَّه/g, 'Allah')
-        .replace(/(اَ?ل)([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, 'a$2-$2$3')
-        // .replace(/([\u0621-\u0652])(اَ?لْ?)/g, '$1\'l-')
-        .replace(/(^|\s)(اَ?لْ?)/g, '$1al-')
-        
-        // replace all other cases of shadda with previous letter
-        .replace(/([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, '$1$1$2')
+      // replace all other cases of shadda with previous letter
+      .replace(/([\u0621-\u064A])([\u064B-\u0652]*)(\u0651)/g, '$1$1$2')
 
-        // remove remaining sukoon
-        .replace(/[ْ]/g, '')
+      // remove remaining sukoon
+      .replace(/[ْ]/g, '')
 
-        // map remaining arabic characters according to transliterationDict
-        .split('')
-        .map(char => transliterationDict[char]?.[0] || char)
-        .join('')
+      // map remaining arabic characters according to transliterationDict
+      .split('')
+      .map(char => transliterationDict[char]?.[0] || char)
+      .join('')
+  }
 
-      // might want to post-parse weird vowel clusters as a kludge
-    } else { return '' }
-
-  });
+  return textNodes.map(arabicText => ({ ...arabicText, text: transliterate(arabicText.text) }));
 }
 
 // TODO: joined vowels -- fii instead of fiy, etc
