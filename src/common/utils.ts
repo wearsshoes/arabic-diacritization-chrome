98118c1a113adce200c01d639731bfb54520e200
Rachel Shu <rachel@rachelshu.com>
2024-05-09 14:22:13 -0600

    simplified calculateHash

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,28 +1,18 @@
 // utils.ts
 import xxhash from 'xxhash-wasm'
 
-export async function calculateHash(input: string): Promise<string>;
-export async function calculateHash(input: string[]): Promise<string[]>;
-export async function calculateHash(input: string | string[]): Promise<string | string[]> {
-  // Creates the WebAssembly instance.
+export async function calculateHash(input: string): Promise<string>
+export async function calculateHash(input: string[]): Promise<string[]>
+export async function calculateHash(input: string | string[]): Promise< string | string[]> {
   const hasher = await xxhash();
-
-  // Calculates the 64-bit hash.
-  if (Array.isArray(input)) {
-    const responseArray: string[] = []
-    input.forEach(inputString => {
-      if (inputString === '') {
-        responseArray.push('')
-      } else {
-        const hash64Hex = hasher.h64ToString(inputString);
-        responseArray.push(hash64Hex);
-      }
-    });
-    return responseArray;
-  } else {
-    const hash64Hex = hasher.h64ToString(input);
-    return hash64Hex;
+  if (typeof input === 'string') {
+    if (input === '') { return '' }
+    else { return hasher.h64ToString(input) }
   }
+  return (input.map((inputText) => {
+    if (inputText === '') { return '' }
+    else { return hasher.h64ToString(inputText) }
+  }));
 }
 
 export const sentenceRegex = /[.!?؟]+\s*\n*/g;
