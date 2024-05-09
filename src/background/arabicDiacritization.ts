354b66b81933c71f3d62c4c0eec09c297637f7f8
Rachel Shu <rachel@rachelshu.com>
2024-05-09 14:29:39 -0600

    rename diacritization.ts to processTextNodes, split arabicDiacritization out

--- src/background/arabicDiacritization.ts
+++ src/background/arabicDiacritization.ts
@@ -1,1 +1,127 @@
+import Anthropic from "@anthropic-ai/sdk";
 import { TextNode } from '../common/webpageDataClass';
+import { Claude, countSysPromptTokens, anthropicAPICall, constructAnthropicMessage} from "./anthropicCaller";
+import { messageContentScript } from './background';
+import { sentenceRegex } from '../common/utils';
+import { Prompt } from '../common/types'
+import prompts from './defaultPrompts.json';
+
+const defaultPrompt = prompts[1];
+
+async function getPrompt(): Promise<Prompt> {
+  try {
+    const { selectedPrompt } = await chrome.storage.sync.get('selectedPrompt');
+    return selectedPrompt;
+  } catch (error) {
+    console.error(`Error retrieving prompt: ${error}, using default prompt.`);
+    return defaultPrompt;
+  }
+}
+
+// Full diacritization
+export async function fullDiacritization(tabId: number, selectedNodes: TextNode[]): Promise<TextNode[]> {
+  const diacritizationBatches = createDiacritizationElementBatches(selectedNodes, 750);
+  const prompt = await getPrompt();
+  const claude = new Claude()
+  const sysPromptLength = await countSysPromptTokens(prompt.text) || 0;
+  const maxTries = 1;
+  const delimiter = '|';
+
+  messageContentScript(tabId, { action: 'diacritizationBatchesStarted', batches: diacritizationBatches.length });
+
+  // diacritize the texts in parallel with retries
+  const diacritizedNodes = await Promise.all(
+    diacritizationBatches.map(async (originals) => {
+      const originalText = originals.flatMap((textNode) => textNode.text.replace(delimiter, '')).join(delimiter);
+
+      const msg = constructAnthropicMessage(originalText, prompt, claude);
+
+      for (let tries = 0; tries < maxTries; tries++) {
+        claude.escalateModel();
+        try {
+          const response: Anthropic.Message = await anthropicAPICall(msg, claude.apiKey);
+          const diacritizedText: string = response.content[0].text;
+          console.log('originals: ', originalText, 'diacritized: ', diacritizedText);
+          const validResponse = validateResponse(response, originalText, diacritizedText);
+
+          if (validResponse) {
+            const replacements: TextNode[] = diacritizedText.split(delimiter).map((text, index) => ({ ...originals[index], text }));
+            messageContentScript(tabId, { action: 'diacritizationChunkFinished', originals, replacements, method: 'fullDiacritics' });
+            return replacements;
+          }
+        } catch (error) {
+          console.error('Error diacritizing chunk:', error);
+        }
+        if (tries < maxTries - 1) {
+          console.log('Failed validation. trying again: try', tries + 1, 'of', maxTries);
+        }
+      }
+      console.error('Failed to diacritize chunk, returning original text');
+      return originals;
+    })
+  ).then((result) => {return result.flat()});
+
+  console.log('Diacritized text:', diacritizedNodes);
+  return diacritizedNodes;
+
+  function validateResponse(response: Anthropic.Messages.Message, originalText: string, diacritizedText: string): boolean {
+    const { input_tokens, output_tokens } = response.usage;
+    console.log('Input tokens:', input_tokens - sysPromptLength, 'Output tokens:', output_tokens);
+    const enoughTokens = output_tokens > (input_tokens - sysPromptLength);
+
+    // check if the diacritized text is longer than the original text
+    const separatorsInOriginal = originalText.split(delimiter).length - 1;
+    const separatorsInDiacritized = diacritizedText.split(delimiter).length - 1;
+    console.log('Separators in original:', separatorsInOriginal, 'Separators in diacritized:', separatorsInDiacritized);
+    const rightDelimiters = (separatorsInDiacritized - separatorsInOriginal === 0);
+    return (enoughTokens && rightDelimiters);
+  }
+}
+
+// Create batches of elements according to sentence boundaries and API character limit.
+function createDiacritizationElementBatches(textElements: TextNode[], maxChars: number): TextNode[][] {
+  console.log('starting batching on', textElements.length, 'elements');
+  const textElementBatches: TextNode[][] = [];
+  let currentBatch: TextNode[] = [];
+  let currentBatchLength = 0;
+  const batchStats: [number, string, TextNode[]][] = [];
+
+  textElements.forEach((textElement, index) => {
+    const text = textElement.text;
+    if (text != '') {
+      // Check whether there are any Arabic characters. Not used
+      // function containsArabicCharacters(text: string): boolean {
+      //   const arabicRegex = /[\u0600-\u06FF]/;
+      //   return arabicRegex.test(text);
+      // }
+      // if (containsArabicCharacters(text)) {
+      // we want to take these out, but doing so might cause us to lose context within sentences.
+      // once we have better batch management with sentences paragraphs etc, we can then address this.
+      const textLength = text.length;
+
+      if ((currentBatchLength + textLength) > maxChars) {
+        if (currentBatch.length > 0) {
+          batchStats.push([currentBatchLength, 'maxChars', currentBatch]);
+          textElementBatches.push(currentBatch);
+        }
+        currentBatch = [textElement];
+        currentBatchLength = textLength;
+      } else {
+        currentBatch.push(textElement);
+        currentBatchLength += textLength;
+
+        // handle sentence breaks as new batch
+        if ((text.match(sentenceRegex) && (currentBatchLength > (maxChars * 2 / 3))) || index === (textElements.length - 1)) {
+          batchStats.push([currentBatchLength, 'end of sentence', currentBatch]);
+          textElementBatches.push(currentBatch);
+          currentBatch = [];
+          currentBatchLength = 0;
+        }
+      }
+    }
+  });
+  console.log(`batches created: ${textElementBatches.length}`, batchStats);
+
+  return textElementBatches;
+}
+
--- src/background/processTextNodes.ts
+++ src/background/processTextNodes.ts
@@ -1,1 +1,143 @@
+import { AppResponse } from '../common/types';
 import { PageMetadata, TextNode, WebPageDiacritizationData } from "../common/webpageDataClass";
+import { arabicToArabizi } from "./arabizi";
+import { fullDiacritization } from "./arabicDiacritization";
+import { messageContentScript, dataManager } from './background';
+
+export async function processSelectedText(tab: chrome.tabs.Tab, method: string = 'fullDiacritics'): Promise<void> {
+  if (!tab.id) return;
+  const request = await messageContentScript(tab.id, { action: "getSelectedNodes" });
+  if (request.status === 'error') {
+    console.error('Error getting selected nodes:', request.error);
+    return;
+  }
+  const { selectedNodes = [] } = request;
+  if (selectedNodes.length === 0) {
+    console.log("No text selected, processing full webpage.");
+    await processFullWebpage(tab, method);
+    return;
+  }
+  console.log("Doing ", method, "for selected nodes:", selectedNodes);
+  let replacementText = await fullDiacritization(tab.id, selectedNodes);
+  console.log('Diacritization result:', replacementText);
+  if (method === 'arabizi') {
+    replacementText = arabicToArabizi(replacementText);
+  }
+  await messageContentScript(tab.id, { action: 'updateWebsiteText', originals: selectedNodes, replacements: replacementText, method });
+}
+
+export async function processFullWebpage(tab: chrome.tabs.Tab, method: string): Promise<AppResponse> {
+
+  if (!tab.id || !tab.url) return ({ status: 'error', error: new Error('No tab ID or URL found') });
+  const tabId = tab.id;
+
+  const response = await messageContentScript(tab.id, { action: 'getWebsiteMetadata' });
+  if (response.status === 'error') {
+    return response;
+  }
+  const { pageMetadata }: { pageMetadata?: PageMetadata; } = response;
+  if (!pageMetadata) {
+    return ({ status: 'error', error: new Error('Did not recieve pageMetadata from content') });
+  }
+  const webPageDiacritizationData = await WebPageDiacritizationData.build(pageMetadata);
+  console.log('Website metadata:', pageMetadata);
+
+  // Load the saved data for the current webpage
+  const retrievedPageData = await dataManager.getWebPageData(tab.url);
+  console.log('Retrieved page data:', retrievedPageData);
+
+  let userMessage: string = '';
+
+  const requestMatchesSaved = function (): boolean {
+    // If there's no saved data, continue
+    if (!retrievedPageData) {
+      userMessage = "There's no saved webpage data, continuing";
+      return false;
+    }
+
+    // If saved data is available but has changed, ignore old data, continue
+    const oldSig = retrievedPageData.metadata.contentSignature;
+    const currentSig = pageMetadata.contentSignature;
+    if (oldSig !== currentSig) {
+      userMessage = `Content has changed, will regenerate diacritization and discard all old ones. Current hash: ${currentSig}, Saved hash: ${oldSig}`;
+      return false;
+      // TODO: only update the elements that have changed
+      // logChanges(retrievedPageData.metadata, pageMetadata);
+    }
+
+    // If saved data doesn't contain the requested diacritization method, retrieve all other saved diacritizations, continue
+    if (!Object.hasOwn(retrievedPageData.diacritizations, method)) {
+      webPageDiacritizationData.diacritizations = retrievedPageData.diacritizations;
+      userMessage = `Webpage is unchanged, generating ${method} from saved data: ${retrievedPageData.diacritizations}`;
+      return false;
+    }
+
+    // If saved data contains the requested method, update the website text with the saved data, stop
+    const original = retrievedPageData.getDiacritization('original');
+    const diacritization = retrievedPageData.getDiacritization(method);
+    messageContentScript(tabId, { action: 'updateWebsiteText', originals: original, replacements: diacritization, method });
+    userMessage = `Webpage is unchanged, using saved ${method} data`;
+    return true;
+  };
+
+  // Stop here if we've already processed the request.
+  if (requestMatchesSaved()) {
+    return ({ status: 'success', userMessage });
+  }
+
+  // Get the website text
+  if (!webPageDiacritizationData.diacritizations['original']) {
+    const response = await messageContentScript(tab.id, { action: 'getWebsiteText' });
+    if (response.status === 'error') {
+      return response;
+    }
+    const { selectedNodes } = response;
+    if (!selectedNodes) {
+      return ({ status: 'error', error: new Error('Did not recieve TextNodes array from content script.') });
+    }
+    console.log('Retrieved original website text:', selectedNodes);
+    await webPageDiacritizationData.createOriginal(selectedNodes);
+  }
+
+  // Process the webpage
+  switch (method) {
+    // If the method is 'fullDiacritics' and saved data exists for the current webpage, return the saved results
+    case 'fullDiacritics':
+      await fullDiacritization(tabId, webPageDiacritizationData.getDiacritization('original'))
+        .then((fullDiacritics) => {
+          webPageDiacritizationData.addDiacritization(fullDiacritics, method);
+        });
+      break;
+
+    case 'arabizi': {
+      if (!webPageDiacritizationData.diacritizations['fullDiacritics']) {
+        console.log("Full diacritization doesn't exist, Diacritizing text first");
+        await fullDiacritization(tabId, webPageDiacritizationData.getDiacritization('original'))
+          .then((fullDiacritics) => {
+            webPageDiacritizationData.addDiacritization(fullDiacritics, 'fullDiacritics');
+          });
+      }
+
+      const arabiziNodes: TextNode[] = arabicToArabizi(webPageDiacritizationData.getDiacritization('fullDiacritics'));
+       webPageDiacritizationData.addDiacritization(arabiziNodes, method);
+      break;
+    }
+
+    default:
+      throw new Error(method + ' is not implemented yet');
+  }
+
+  // Update the saved metadata
+  console.log('Updating saved web page data');
+  dataManager.updateWebPageData(tab.url, webPageDiacritizationData)
+    .then(() => console.log('Saved webpage data updated:', webPageDiacritizationData))
+    .catch((error) => console.error('Failed to update saved webpage data:', error));
+
+  // Update the website text
+  const original = webPageDiacritizationData.getDiacritization('original');
+  const diacritization = webPageDiacritizationData.getDiacritization(method);
+  messageContentScript(tab.id, { action: 'updateWebsiteText', originals: original, replacements: diacritization, method })
+    .then(() => console.log('Website text updated'));
+
+  return ({ status: 'success' });
+}
