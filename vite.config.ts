98efb9d5a44b602a9dda6d75c47f44360d601e97
Rachel Shu <rachel@rachelshu.com>
2024-05-23 13:36:24 -0700

    use crxjs `defineManifest` in order to make HMR work better; delete manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,56 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { crx, defineManifest } from '@crxjs/vite-plugin';
 
-// https://vitejs.dev/config/
+// Define the manifest dynamically
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "ArabEasy",
+  version: "0.4.1",
+  description: "Allows users to add pronunciation diacritics (taškīl) to Arabic text or convert Arabic text to Arabizi.",
+  permissions: [
+    "activeTab",
+    "tabs",
+    "contextMenus",
+    "storage",
+    "scripting",
+    "commands",
+    "debugger"
+  ],
+  background: {
+    service_worker: "src/background/background.ts"
+  },
+  options_ui: {
+    page: "src/options/optionsPage.html",
+    open_in_tab: true
+  },
+  content_scripts: [
+    {
+      matches: ["<all_urls>"],
+      js: ["src/content/content.tsx"]
+    }
+  ],
+  content_security_policy: {
+    extension_pages: "script-src 'self' 'wasm-unsafe-eval'; object-src 'self';"
+  },
+  action: {
+    default_popup: "src/popup/extensionPopup.html",
+    default_title: "ArabEasy"
+  },
+  commands: {
+    "toggle-widget": {
+      suggested_key: {
+        default: "Ctrl+Shift+U",
+        mac: "Command+Shift+U"
+      },
+      description: "Toggle the widget."
+    }
+  },
+  host_permissions: [
+    "<all_urls>"
+  ]
+});
+
 export default defineConfig({
   plugins: [
     react(),
