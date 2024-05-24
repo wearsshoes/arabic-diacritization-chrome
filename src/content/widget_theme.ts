d84e23871dc120a580d3870a3cefe957357acdc1
Rachel Shu <rachel@rachelshu.com>
2024-05-23 21:01:26 -0700

    create widget_theme.ts

---
+++ src/content/widget_theme.ts
@@ -0,0 +1,96 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    styles: {
+      global: {
+        'html, body': {
+          fontSize: 'inherit',  // Ensure Chakra uses the inherited font-size from the shadow DOM
+        },
+      }
+    },
+    fontSizes: {
+      xs: "12px",
+      sm: "14px",
+      md: "16px",
+      lg: "18px",
+      xl: "20px",
+      "2xl": "24px",
+      "3xl": "30px",
+      "4xl": "36px",
+      "5xl": "48px",
+      "6xl": "60px",
+      "7xl": "72px",
+      "8xl": "96px",
+      "9xl": "128px",
+    },
+    sizes: {
+      max: 'max-content',
+      min: 'min-content',
+      full: '100%',
+      '3xs': '224px',
+      '2xs': '256px',
+      xs: '320px',
+      sm: '384px',
+      md: '448px',
+      lg: '512px',
+      xl: '576px',
+      '2xl': '672px',
+      '3xl': '768px',
+      '4xl': '896px',
+      '5xl': '1024px',
+      '6xl': '1152px',
+      '7xl': '1280px',
+      '8xl': '1440px',
+      "1": "4px",
+      "2": "8px",
+      "3": "12px",
+      "4": "16px",
+      "5": "20px",
+      "6": "24px",
+      "7": "28px",
+      "8": "32px",
+      "9": "36px",
+      "10": "40px",
+      "12": "48px",
+      "14": "56px",
+      "16": "64px",
+      "20": "80px",
+      "24": "96px",
+      "28": "112px",
+      "32": "128px",
+      "36": "144px",
+      "40": "160px",
+      "44": "176px",
+      "48": "192px",
+      "52": "208px",
+      "56": "224px",
+      "60": "240px",
+      "64": "256px",
+      "72": "288px",
+      "80": "320px",
+      "96": "384px",
+      "px": "1px",
+      "0-5": "2px",
+      "1-5": "6px",
+      "2-5": "10px",
+      "3-5": "14px",
+      container: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
+    components: {
+      Container: {
+        fontSize: '16px',
+      },
+      Text: {
+        baseStyle: {
+          color: 'red',
+        },
+      },
+    },
+  });
+
+export default theme;
