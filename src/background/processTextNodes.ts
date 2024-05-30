b9aae01b2a45c815dc20ed30afbbea07edc9f64a
Rachel Shu <rachel@rachelshu.com>
2024-05-29 18:00:49 -0700

    remove old checkSave, refactor new checkSave from processWebpage

--- src/background/processTextNodes.ts
+++ src/background/processTextNodes.ts
@@ -1,7 +1,6 @@
 import { AppResponse } from '../common/types';
-import { WebpageDiacritizationData } from "../common/webpageDataClass";
+import { TextNode, WebpageDiacritizationData } from "../common/webpageDataClass";
 import { fullDiacritization } from "./arabicDiacritization";
-import { arabicToArabizi } from './arabizi';
 import { messageContentScript } from './background';
 
 export async function processSelectedText(tab: chrome.tabs.Tab, method: string = 'fullDiacritics'): Promise<void> {
@@ -36,54 +35,26 @@ export async function processWebpage(tab: chrome.tabs.Tab, method: string): Prom
 
     const latest = await messageContentScript(tab.id, { action: 'getWebsiteText' });
     if (latest.status === 'error') return latest;
-    const { pageMetadata, selectedNodes } = latest;
-    if (!pageMetadata) throw new Error('No page metadata found');
+
+    const { contentSignature, selectedNodes } = latest;
+    if (!contentSignature) throw new Error('No page metadata found');
     if (!selectedNodes) throw new Error('No selected nodes found');
-    const webpageDiacritizationData = await WebpageDiacritizationData.build(tabUrl, pageMetadata.contentSignature);
-    await webpageDiacritizationData.createOriginal(selectedNodes);
 
-    // const doneThatAlready = await checkSaves(webpageDiacritizationData, method, tab)
-    // if (doneThatAlready) return ({ status: 'success', userMessage: 'yay it existed' });
+    const { diacritics, webpageDiacritizationData }: { diacritics: TextNode[]; webpageDiacritizationData: WebpageDiacritizationData; } = await checkSave(tabUrl, contentSignature, method, selectedNodes, tabId);
 
-    // Process the webpage
-    console.log('Processing webpage:', tabUrl, 'with method:', method)
-    switch (method) {
-      case 'original': {
-        const original = webpageDiacritizationData.getDiacritization('original');
-        messageContentScript(tabId, { action: 'updateWebsiteText', tabUrl, replacements: Array.from(original), method });
-        break;
-      }
-      case 'fullDiacritics': {
-        await fullDiacritization(tabId, tabUrl, webpageDiacritizationData.getDiacritization('original'))
-          .then((result) => {
-            webpageDiacritizationData.addDiacritization(result, method);
-          })
-        break;
-      }
-      case 'arabizi': {
-        if (!webpageDiacritizationData.getDiacritization('fullDiacritics')) {
-          console.log("Full diacritization doesn't exist, Diacritizing text first");
-          await fullDiacritization(tabId, tabUrl, webpageDiacritizationData.getDiacritization('original'), true)
-            .then((result) => {
-              webpageDiacritizationData.addDiacritization(result, 'fullDiacritics');
-            });
-        } else {
-          console.log("Full diacritization exists, using it for Arabizi conversion");
-          const replacements = arabicToArabizi(webpageDiacritizationData.getDiacritization('fullDiacritics'));
-            messageContentScript(tabId, { action: 'updateWebsiteText', tabUrl, replacements, method });
-        }
-        break;
-      }
-
-      default:
-        throw new Error(method + ' is not implemented yet');
-    }
+    messageContentScript(tabId, {
+      action: 'updateWebsiteText',
+      tabUrl: tab.url,
+      replacements: diacritics,
+      method,
+      ruby: method === 'arabizi'
+    });
 
     // Update the saved metadata
     let message = {} as AppResponse;
     chrome.storage.local.set({ [tabUrl]: webpageDiacritizationData })
       .then(() => {
-        chrome.storage.local.get(tabUrl, (data) => console.log('Saved webpage data:', data));
+        chrome.storage.local.get(tabUrl, (data) => console.log('Successfully saved webpage data:', data));
         message = { status: 'success', userMessage: 'Webpage diacritization complete.' };
       })
       .catch((error: Error) => {
@@ -93,6 +64,7 @@ export async function processWebpage(tab: chrome.tabs.Tab, method: string): Prom
 
     messageContentScript(tabId, { action: 'allDone' });
     return message;
+
   } catch (error) {
     if (error instanceof Error) {
       if (error.name === 'AbortError') {
@@ -106,61 +78,24 @@ export async function processWebpage(tab: chrome.tabs.Tab, method: string): Prom
   return ({ status: 'error', errorMessage: 'Unknown error occurred' });
 }
 
-// async function checkSaves(data: WebpageDiacritizationData, method: string, tab: chrome.tabs.Tab): Promise<boolean> {
-//   // Load the saved data for the current webpage
-//   const { id: tabId, url: tabUrl } = tab;
-//   if (!tabId || !tabUrl) return false;
+async function checkSave(tabUrl: string, contentSignature: string, method: string, selectedNodes: TextNode[], tabId: number) {
+  let webpageDiacritizationData: WebpageDiacritizationData;
+  let diacritics: TextNode[] = [];
 
-//   const retrievedPageData = await chrome.storage.local.get(tabUrl);
-//   if (!retrievedPageData) return false;
+  const saved: WebpageDiacritizationData = Object(await chrome.storage.local.get(tabUrl))[tabUrl];
+  if (saved && saved.contentSignature === contentSignature) {
+    console.log('Using saved webpage data:');
+    Object.setPrototypeOf(saved, WebpageDiacritizationData.prototype);
+    webpageDiacritizationData = saved;
+    webpageDiacritizationData.updateLastVisited(new Date());
+    diacritics = webpageDiacritizationData.getDiacritization(method === 'original' ? 'original' : 'fullDiacritics');
+  } else {
+    console.log('Content has changed, creating new webpage data:');
+    webpageDiacritizationData = await WebpageDiacritizationData.build(tabUrl, contentSignature);
+    webpageDiacritizationData.createOriginal(selectedNodes);
+    diacritics = await fullDiacritization(tabId, tabUrl, selectedNodes, method === 'arabizi');
+    webpageDiacritizationData.updateDiacritization(diacritics, 'fullDiacritics');
+  }
+  return { diacritics, webpageDiacritizationData };
+}
 
-//   console.log('Retrieved page data:', retrievedPageData);
-//   const same = retrievedPageData.contentSignature === data.contentSignature
-//   const fr = Object.hasOwn(retrievedPageData.diacritizations, method);
-//   if (same && fr) {
-//     const diacritization = retrievedPageData.getDiacritization(method);
-//     console.log(`Using saved data for ${method}`, diacritization);
-//     messageContentScript(tabId, { action: 'updateWebsiteText', tabUrl: tab.url, replacements: Array.from(diacritization), method });
-//     return true;
-//   }
-//   return false;
-// }
-
-// let userMessage: string = '';
-
-// const requestMatchesSaved = function (): boolean {
-// If there's no saved data, continue
-// if (!retrievedPageData) {
-//   userMessage = "There's no saved webpage data, continuing";
-//   return false;
-// }
-// }
-
-//   // If saved data is available but has changed, ignore old data, continue
-//   const oldSig = retrievedPageData.metadata.contentSignature;
-//   const currentSig = pageMetadata.contentSignature;
-//   if (oldSig !== currentSig) {
-//     userMessage = `Content has changed, will regenerate diacritization and discard all old ones. Current hash: ${currentSig}, Saved hash: ${oldSig}`;
-//     return false;
-//     // TODO: only update the elements that have changed
-//     // logChanges(retrievedPageData.metadata, pageMetadata);
-//   }
-
-//   // If saved data doesn't contain the requested diacritization method, retrieve all other saved diacritizations, continue
-//   if (!Object.hasOwn(retrievedPageData.diacritizations, method)) {
-//     webpageDiacritizationData.diacritizations = retrievedPageData.diacritizations;
-//     userMessage = `Webpage is unchanged, generating ${method} from saved data: ${retrievedPageData.diacritizations}`;
-//     return false;
-//   }
-
-//   // If saved data contains the requested method, update the website text with the saved data, stop
-//   const diacritization = retrievedPageData.getDiacritization(method);
-//   messageContentScript(tabId, { action: 'updateWebsiteText', tabUrl: tab.url, replacements: diacritization, method });
-//   userMessage = `Webpage is unchanged, using saved ${method} data`;
-//   return true;
-// };
-
-// // Stop here if we've already processed the request.
-// if (requestMatchesSaved()) {
-//   return ({ status: 'success', userMessage });
-// }
