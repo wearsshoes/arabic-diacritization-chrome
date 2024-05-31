c6a9377c490588a8622b71cf37e37ef7be1d8829
Rachel Shu <rachel@rachelshu.com>
2024-05-31 13:29:52 -0700

    update options page towards figma

--- src/options/optionsPage.tsx
+++ src/options/optionsPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import LLMOptions from './llmOptions';
@@ -6,71 +5,213 @@ import APIKeyForm from './apiKeyForm';
 import DataManagement from './dataManagement';
 
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../assets/theme';
+
+
+import React, { ReactNode, useState } from 'react';
+import {
+  IconButton,
+  Box,
+  CloseButton,
+  Flex,
+  Icon,
+  useColorModeValue,
+  Link,
+  Drawer,
+  DrawerContent,
+  Heading,
+  useDisclosure,
+  BoxProps,
+  FlexProps,
+} from '@chakra-ui/react';
 
 import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box
-} from '@chakra-ui/react'
+  FiPenTool,
+  FiSave,
+  FiSettings,
+  FiActivity,
+  FiMenu
+} from 'react-icons/fi';
 
-/* eslint-disable react-refresh/only-export-components */
-const Options: React.FC = () => {
+import { IconType } from 'react-icons';
+
+interface LinkItemProps {
+  name: string;
+  icon: IconType;
+}
+const LinkItems: Array<LinkItemProps> = [
+  { name: 'General', icon: FiSettings },
+  { name: 'Custom Prompt', icon: FiPenTool },
+  { name: 'Local Data', icon: FiSave },
+  { name: 'API Usage', icon: FiActivity },
+];
+
+export default function SimpleSidebar({ children }: { children: ReactNode }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedItem, setSelectedItem] = useState('General');
+
   return (
-    <Box width='60%' margin='auto' paddingTop={'50px'}>
-      <Accordion allowToggle>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as='span' flex='1' textAlign='left'>
-                LLM Options
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <LLMOptions />
-          </AccordionPanel>
-        </AccordionItem>
+    <Box>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: 'none', md: 'block' }}
+        onItemClick={(item) => setSelectedItem(item)}
+      />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full">
+        <DrawerContent>
+          <SidebarContent onClose={onClose} onItemClick={() => { }} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
 
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as='span' flex='1' textAlign='left'>
-                API Key
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <APIKeyForm />
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as='span' flex='1' textAlign='left'>
-                Data Management
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <DataManagement />
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
+      <Box ml={{ base: 0, md: 60 }} p="10" maxWidth={"800px"}>
+        <Heading size="2xl" pb="10" fontWeight={"100"} textTransform={"uppercase"}>{selectedItem ?? "OPTIONS"}</Heading>
+        {selectedItem === 'General' ? <APIKeyForm /> : children}
+        {selectedItem === 'Custom Prompt' ? <LLMOptions /> : children}
+        {selectedItem === 'Local Data' ? <DataManagement /> : children}
+      </Box>
     </Box>
+  );
+}
+
+interface SidebarProps extends BoxProps {
+  onClose: () => void;
+  onItemClick: (item: string) => void;
+}
+
+
+const SidebarContent = ({ onClose, onItemClick, ...rest }: SidebarProps) => {
+  return (
+    <Box
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      borderRight="1px"
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      <Flex h="220" alignItems="center" mx="4" justifyContent="space-between">
+        <Box
+          bg="white"
+          paddingX="20px"
+          paddingY="20px"
+          paddingTop={"40px"}
+          paddingRight={"32px"}
+          borderColor="#000000"
+          borderStartWidth="1px"
+          borderEndWidth="1px"
+          borderTopWidth="1px"
+          borderBottomWidth="1px"
+        >
+          <Heading
+            lineHeight="1.2"
+            fontWeight="black"
+            fontSize="36px"
+            textTransform="uppercase"
+            color="#000000"
+          >
+            Easy<br />Peasy<br />Arabizi
+          </Heading>
+        </Box>
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem
+          key={link.name}
+          icon={link.icon}
+          onClick={() => onItemClick(link.name)}
+        >
+          {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  );
+};
+
+
+
+interface NavItemProps extends FlexProps {
+  icon: IconType;
+  children: ReactNode;
+  onClick: () => void;
+}
+
+const NavItem = ({ icon, children, onClick, ...rest }: NavItemProps) => {
+  return (
+    <Link href="#" variant="menu" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }} onClick={onClick}>
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'cyan.400',
+          color: 'white',
+        }}
+        {...rest}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: 'white',
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
+  );
+};
+
+interface MobileProps extends FlexProps {
+  onOpen: () => void;
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent="flex-start"
+
+      {...rest}>
+      <IconButton
+        variant="outline"
+        onClick={onOpen}
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
+      <Heading
+        fontWeight="black"
+        textTransform="uppercase"
+        paddingLeft="20px"
+      >
+        Easy Peasy Arabizi
+      </Heading>
+    </Flex>
   );
 };
 
 ReactDOM.createRoot(document.getElementById('options-root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Options />
+    <ChakraProvider theme={theme}>
+      <SimpleSidebar children={undefined}></SimpleSidebar>
     </ChakraProvider>
   </React.StrictMode>,
 )
